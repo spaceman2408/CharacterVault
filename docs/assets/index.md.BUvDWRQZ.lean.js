9ebe21e3e28fac00cbefdc6d80c3c261c6bd81a1
spaceman2408 <dankanimeguy@gmail.com>
2026-08-23 17:58:32 -0400

    Deploy GitHub Pages

---
+++ docs/assets/index.md.BUvDWRQZ.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as r,ag as o}from"./chunks/framework.qXLDknWC.js";const g=JSON.parse('{"title":"CharacterVault","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),n={name:"index.md"};function i(l,a,s,c,d,h){return e(),r("div",null,[...a[0]||(a[0]=[o("",8)])])}const p=t(n,[["render",i]]);export{g as __pageData,p as default};
